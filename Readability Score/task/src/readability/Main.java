ae1d18e9aa2d8d9f8e7d1fcb7018f0d6c74cdd60
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2022-12-16 16:24:37 +0100

    add project

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -2,16 +2,8 @@ package readability;
 
 import java.util.Scanner;
 
-/**
- * The type Main.
- */
 public class Main {
 
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     */
     public static void main(String[] args) {
         /*
          * stage 2
