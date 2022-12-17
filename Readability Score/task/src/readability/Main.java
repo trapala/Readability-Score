4cea4248966cb8d9a0327d46ed4662c49a90ee63
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2022-12-17 21:24:34 +0100

    add project

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,27 +1,49 @@
 package readability;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
-         * stage 2
+         * stage 3
          */
-        Scanner scanner = new Scanner(System.in);
-        String[] strings = scanner.nextLine().split("[!.?]");
+        String file = Files.readString(Path.of(args[0]));
+        String[] strings = file.split("\\s+");
+        int words = strings.length;
+        int sentences = file.split("[!.?]").length;
+        int characters = file.replaceAll("\\s+", "").length();
+        double score = 4.71 * ((double) characters / words) + 0.5 * ((double) words / sentences) - 21.43;
+        int scoreCeil = (int) Math.ceil(score);
+        String age = switch (scoreCeil) {
+            case 1 -> "5-6";
+            case 2 -> "6-7";
+            case 3 -> "7-8";
+            case 4 -> "8-9";
+            case 5 -> "9-10";
+            case 6 -> "10-11";
+            case 7 -> "11-12";
+            case 8 -> "12-13";
+            case 9 -> "13-14";
+            case 10 -> "14-15";
+            case 11 -> "15-16";
+            case 12 -> "16-17";
+            case 13 -> "17-18";
+            case 14 -> "18-22";
+            default -> null;
+        };
 
-
-        double avg = 0.0;
-        for (int i = 0; i < strings.length; i++) {
-            String[] strArray = strings[i].split("\\s+");
-
-            avg += i == 1 ? strArray.length : strArray.length - 1;
-        }
-
-        double result = avg / strings.length;
-
-        System.out.println(result > 10 ? "HARD" : "EASY");
+        System.out.println("The text is:");
+        System.out.println(file);
+        System.out.println();
+        System.out.println("Words: " + words);
+        System.out.println("Sentences: " + sentences);
+        System.out.println("Characters: " + characters);
+        System.out.printf("The score is: %.2f\n", score);
+        System.out.println("This text should be understood by " + age + " year-olds.");
 
     }
 }
