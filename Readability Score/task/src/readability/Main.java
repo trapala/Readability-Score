2d7c3e1add46e72c7b80648456917549cbca6f0b
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2022-12-20 18:50:42 +0100

    add project

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,49 +1,82 @@
 package readability;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Scanner;
 
 
 public class Main {
+    protected static String readFile(String pathFile) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(pathFile));
+        return scanner.nextLine();
+    }
+
+    public static double ARI(String text) {
+        int amountOfCharacter = characters(text);
+        int amountOfWords = words(text);
+        int amountOfSentences = sentences(text);
+
+        return 4.71 * ((double) amountOfCharacter / amountOfWords) + 0.5 * ((double) amountOfWords / amountOfSentences) - 21.43;
+    }
+
+    protected static int ageText(double score) {
+        int scoreCeil = (int) Math.ceil(score);
+        int age = switch (scoreCeil) {
+            case 1 -> 6;
+            case 2 -> 7;
+            case 3 -> 8;
+            case 4 -> 9;
+            case 5 -> 10;
+            case 6 -> 11;
+            case 7 -> 12;
+            case 8 -> 13;
+            case 9 -> 14;
+            case 10 -> 15;
+            case 11 -> 16;
+            case 12 -> 17;
+            case 13 -> 18;
+            case 14 -> 22;
+            default -> 0;
+        };
+        return age;
+    }
+
+
+    public static int words(String text) {
+        return text.split("\\s+").length;
+    }
+
+    public static int sentences(String text) {
+        return text.split("[!.?]").length;
+    }
+
+    public static int characters(String text) {
+        return text.replaceAll("\\s+", "").length();
+    }
+
+    public static int syllables(String text) {
+        return 0;
+    }
+
+    public static int polysyllables(String text) {
+        return 0;
+    }
 
     public static void main(String[] args) throws IOException {
         /*
          * stage 3
          */
-        String file = Files.readString(Path.of(args[0]));
-        String[] strings = file.split("\\s+");
-        int words = strings.length;
-        int sentences = file.split("[!.?]").length;
-        int characters = file.replaceAll("\\s+", "").length();
-        double score = 4.71 * ((double) characters / words) + 0.5 * ((double) words / sentences) - 21.43;
-        int scoreCeil = (int) Math.ceil(score);
-        String age = switch (scoreCeil) {
-            case 1 -> "5-6";
-            case 2 -> "6-7";
-            case 3 -> "7-8";
-            case 4 -> "8-9";
-            case 5 -> "9-10";
-            case 6 -> "10-11";
-            case 7 -> "11-12";
-            case 8 -> "12-13";
-            case 9 -> "13-14";
-            case 10 -> "14-15";
-            case 11 -> "15-16";
-            case 12 -> "16-17";
-            case 13 -> "17-18";
-            case 14 -> "18-22";
-            default -> null;
-        };
+
+        String text = readFile(args[0]);
 
         System.out.println("The text is:");
-        System.out.println(file);
+        System.out.println(text);
         System.out.println();
-        System.out.println("Words: " + words);
-        System.out.println("Sentences: " + sentences);
-        System.out.println("Characters: " + characters);
-        System.out.printf("The score is: %.2f\n", score);
-        System.out.println("This text should be understood by " + age + " year-olds.");
-
+        System.out.println("Words: " + words(text));
+        System.out.println("Sentences: " + sentences(text));
+        System.out.println("Characters: " + characters(text));
+        System.out.printf("The score is: %.2f\n", ARI(text));
+        System.out.println("This text should be understood by " + ageText(ARI(text)) + " year-olds.");
     }
 }
